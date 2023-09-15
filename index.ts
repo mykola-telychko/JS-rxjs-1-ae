09ec71f0a6afe7f9cd63dc239ef824c955858a03
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-09-15 21:52:09 +0300

    fin

--- index.ts
+++ index.ts
@@ -1,16 +1,16 @@
 import { Observable } from 'rxjs';
 
-const observable = new Observable((observer) => {
+const observable$ = new Observable((observer) => {
   setTimeout(() => {
-    observer.next('Data fetched');
-  }, 1000);
+    observer.next('Observable-data fetched');
+  }, 2000);
 });
 
-const subscription = observable.subscribe({
+const subscription = observable$.subscribe({
   next: (data) => console.log(data),
   error: (error) => console.error(error),
   complete: () => console.log('Complete'),
 });
 
 // To unsubscribe when done
-subscription.unsubscribe();
+// subscription.unsubscribe();// ++ not work
